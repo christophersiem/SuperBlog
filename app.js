55c6313f6828893f7fba0c863e59a244e4a86f08
christophersiem <christopher.siem@gmail.com>
2021-01-11 11:33:31 +0100

    Add vercel config

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ app.use(express.static("public"));
 //for accepting form data:
 app.use(express.urlencoded({ extended: true }));
 
-// app.get("/api", (req, res) => {
-//   res.redirect("/api/blogs");
-// });
+app.get("/api", (req, res) => {
+  res.redirect("/api/blogs");
+});
 
 app.get("/api/about", (req, res) => {
   res.render("about", { title: "About" });
